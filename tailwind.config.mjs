6693f53ca7a8c67e5f4fcbaad9c364683352bed9
Carlos Sosa <carloseduardososagonzalez@gmail.com>
2024-06-16 21:28:35 -0600

    Cambio de color a la pagina principal

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'e9e4f0': '#E9E4F0',
+				'd3cce3': '#D3CCE3',
+				'azul' : '#002060',
+			  },
+		},
 	},
 	plugins: [],
 }
